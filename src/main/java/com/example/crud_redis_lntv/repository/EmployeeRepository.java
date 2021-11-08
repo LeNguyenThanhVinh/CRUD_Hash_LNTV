e3a7059978230d0f9ff5f58f8fe5550be8ac6fbe
ThanhVinh2712 <you@example.com>
2021-11-08 11:45:59 +0700

    crud set commit

--- src/main/java/com/example/crud_redis_lntv/repository/EmployeeRepository.java
+++ src/main/java/com/example/crud_redis_lntv/repository/EmployeeRepository.java
@@ -4,8 +4,10 @@ import com.example.crud_redis_lntv.model.Employee;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Repository
@@ -13,11 +15,13 @@ public class EmployeeRepository {
 //    private HashOperations hashOperations;//crud hash
     private RedisTemplate redisTemplate;
     private ListOperations listOperations;
+    private SetOperations setOperations;
 
     public EmployeeRepository(RedisTemplate redisTemplate) {
 
 //        this.hashOperations = redisTemplate.opsForHash();
 //        this.listOperations = redisTemplate.opsForList();
+        this.setOperations = redisTemplate.opsForSet();
         this.redisTemplate = redisTemplate;
 
     }
@@ -25,28 +29,33 @@ public class EmployeeRepository {
     public void saveEmployee(Employee employee){
 
 //        hashOperations.put("EMPLOYEE", employee.getId(), employee);
-        listOperations.rightPush("EMPLOYEE", employee);
+//        listOperations.rightPush("EMPLOYEE", employee);
+        setOperations.add("EMPLOYEE", employee);
     }
     public List<Employee> findAll(){
 
 //        return hashOperations.values("EMPLOYEE");
-        return listOperations.range("EMPLOYEE", 0, -1);
+//        return listOperations.range("EMPLOYEE", 0, -1);
+        return Arrays.asList((Employee[]) setOperations.members("EMPLOYEE").toArray());
     }
     public Employee findById(Integer id){
 
 //        return (Employee) hashOperations.get("EMPLOYEE", id);
-        List<Employee> list = listOperations.range("EMPLOYEE", 0, listOperations.size("EMPLOYEE"));
-        for (Employee employee : list) {
-            if(employee.getId() == id)
-                return employee;
-        }
-        return null;
+//        List<Employee> list = listOperations.range("EMPLOYEE", 0, listOperations.size("EMPLOYEE"));
+//        for (Employee employee : list) {
+//            if(employee.getId() == id)
+//                return employee;
+//        }
+//        return null;
+        return (Employee) setOperations.intersect("EMPLOYEE", id);
     }
 
     public void update(Employee employee){
         saveEmployee(employee);
     }
     public void delete(Integer id){
-        listOperations.remove("EMPLOYEE", 1, findById(id));
+//        hashOperations.delete("EMPLOYEE", id);
+//        listOperations.remove("EMPLOYEE", 1, findById(id));
+        setOperations.remove("EMPLOYEE_SET", findById(id));
     }
 }
